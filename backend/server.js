ba2521aee7207da4dacf802de797e86433a95dd3
unknown <dlcogbilltech@gmail.com>
2026-06-24 10:43:48 -0400

    Changed configurations to allow local network requests

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,10 @@ app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
 
 app.use( express.json(), express.urlencoded({ extended: true }) );
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors());
 
 require('./routes/capstoneProject.routes')(app);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`The server is running on ${PORT}`);
 });
